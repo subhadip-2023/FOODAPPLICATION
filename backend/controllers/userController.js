3936063fc2d3952d790a3240317e39d7b7e1c91a
subhadip-2023 <subhadippal2k19@gmail.com>
2024-08-03 19:46:20 +0530

    upgraded

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import userModel from "../Schema/userModel.js";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import validator from "validator";
 
 // user Token
