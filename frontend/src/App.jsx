4d8cad073ce30ed3c444137592af109d11924bbb
subhadip-2023 <subhadippal2k19@gmail.com>
2024-08-05 00:11:39 +0530

    Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,17 @@
 // import React from 'react'
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Footer from "./components/Footer/Footer";
-import { useState } from "react";
+import React, {Suspense, useState } from "react";
 import LoginPopup from "./components/LoginPopup/LoginPopup";
 import Verify from "./pages/Verify/Verify";
 import Myorders from "./pages/MyOrders/Myorders";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
+const Home = React.lazy(()=> import("./pages/Home/Home"));
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
   return (
@@ -21,7 +21,7 @@ const App = () => {
       <div className="app">
         <Navbar setShowLogin={setShowLogin} />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Suspense><Home /></Suspense>} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/order" element={<PlaceOrder />} />
           <Route path="/verify" element={<Verify/>} />
